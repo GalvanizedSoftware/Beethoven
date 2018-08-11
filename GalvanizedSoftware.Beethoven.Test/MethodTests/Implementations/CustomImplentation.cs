9f487b278f036734eaab237cb562dd2a6ba9be84
Alan Hauge Andersen <alan@hauge.com>
2018-08-11 22:38:38 +0200

    Added missing file

---
+++ GalvanizedSoftware.Beethoven.Test/MethodTests/Implementations/CustomImplentation.cs
@@ -0,0 +1,20 @@
+﻿// ReSharper disable UnusedMember.Global
+
+namespace GalvanizedSoftware.Beethoven.Test.MethodTests.Implementations
+{
+  class CustomImplentation
+  {
+
+    internal int GetLength(string text1, string text2, int count)
+    {
+      return text1.Length + text2.Length + count;
+    }
+
+    // ReSharper disable once RedundantAssignment
+    internal bool GetLength2(ref int length, string text1, string text2, int count)
+    {
+      length = text1.Length + text2.Length + count;
+      return true;
+    }
+  }
+}
