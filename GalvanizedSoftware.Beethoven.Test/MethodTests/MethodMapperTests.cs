4ae4214f225098e12d0e635da8c5a78ac0d23f1a
Alan Hauge Andersen <alan@hauge.com>
2018-08-11 22:36:41 +0200

    Added MethodMapperTests

--- GalvanizedSoftware.Beethoven.Test/MethodTests/MethodMapperTests.cs
+++ GalvanizedSoftware.Beethoven.Test/MethodTests/MethodMapperTests.cs
@@ -1,9 +1,33 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Reflection;
+using GalvanizedSoftware.Beethoven.Generic.Methods;
+using GalvanizedSoftware.Beethoven.Test.MethodTests.Implementations;
+using GalvanizedSoftware.Beethoven.Test.MethodTests.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GalvanizedSoftware.Beethoven.Test.MethodTests
 {
   [TestClass]
-  class MethodMapperTests
+  public class MethodMapperTests
   {
+    [TestMethod]
+    public void MethodMapperSimpleTest1()
+    {
+      CustomImplentation customImplentation = new CustomImplentation();
+      BeethovenFactory beethovenFactory = new BeethovenFactory();
+      ITestMethods instance = beethovenFactory.Generate<ITestMethods>(
+       new MappedMethod(nameof(ITestMethods.WithParameters), customImplentation, nameof(customImplentation.GetLength)));
+      Assert.AreEqual(10, instance.WithParameters("w", "sd", 7));
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(TargetParameterCountException))]
+    public void MethodMapperSimpleTest2()
+    {
+      CustomImplentation customImplentation = new CustomImplentation();
+      BeethovenFactory beethovenFactory = new BeethovenFactory();
+      ITestMethods instance = beethovenFactory.Generate<ITestMethods>(
+        new MappedMethod(nameof(ITestMethods.WithParameters), customImplentation, nameof(customImplentation.GetLength2)));
+      Assert.AreEqual(10, instance.WithParameters("w", "sd", 7));
+    }
   }
 }
