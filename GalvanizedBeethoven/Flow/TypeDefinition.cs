2c8ebff4d599ae7adffc5001a34856eaada8a8e1
Alan Hauge Andersen <alan@hauge.com>
2019-03-21 19:57:37 +0100

    Added TypeDefinition for more flow-like coding

---
+++ GalvanizedBeethoven/Flow/TypeDefinition.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace GalvanizedSoftware.Beethoven.Flow
+{
+  public class TypeDefinition<T> where T : class
+  {
+    private readonly BeethovenFactory beethovenFactory = new BeethovenFactory();
+    private readonly object[] implementationObjects;
+
+    public TypeDefinition(params object[] newImplementationObjects)
+    {
+      implementationObjects = newImplementationObjects;
+    }
+
+    private TypeDefinition(TypeDefinition<T> previousDefinition, object[] newImplementationObjects)
+    {
+      beethovenFactory = previousDefinition.beethovenFactory;
+      implementationObjects = previousDefinition.implementationObjects.Concat(newImplementationObjects).ToArray();
+    }
+
+    public TypeDefinition<T> Add(params object[] newImplementationObjects) =>
+      new TypeDefinition<T>(this, newImplementationObjects);
+
+    public T Create() => beethovenFactory.Generate<T>(implementationObjects);
+  }
+}
