900ab6b5943e5226baea4099e527e9d855ee8024
Alan Hauge Andersen <alan@hauge.com>
2019-03-21 19:56:59 +0100

    null check

--- GalvanizedBeethoven/Generic/Properties/SkipIfSame.cs
+++ GalvanizedBeethoven/Generic/Properties/SkipIfSame.cs
@@ -13,7 +13,12 @@ namespace GalvanizedSoftware.Beethoven.Generic.Properties
 
     public bool InvokeSetter(T newValue)
     {
-      if (newValue.Equals(oldValue))
+      if (newValue == null)
+      {
+        if (oldValue == null)
+          return false;
+      }
+      else if (newValue.Equals(oldValue))
         return false;
       oldValue = newValue;
       return true;
