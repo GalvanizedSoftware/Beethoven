6885b00f8ad412636a9d0351cce17e0309627247
Alan Hauge Andersen <alan@hauge.com>
2019-03-23 15:58:36 +0100

    "Flow" corrected to "fluent"

---
+++ GalvanizedBeethoven/Fluent/TypeDefinition.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GalvanizedSoftware.Beethoven.Generic.Events;
+
+namespace GalvanizedSoftware.Beethoven.Fluent
+{
+  public class TypeDefinition<T> where T : class
+  {
+    private readonly BeethovenFactory beethovenFactory = new BeethovenFactory();
+    private readonly object[] implementationObjects;
+    private readonly List<(string, Action<IEventTrigger>)> eventList = new List<(string, Action<IEventTrigger>)>();
+
+    public TypeDefinition(params object[] newImplementationObjects)
+    {
+      implementationObjects = newImplementationObjects;
+    }
+
+    private TypeDefinition(TypeDefinition<T> previousDefinition, object[] newImplementationObjects)
+    {
+      beethovenFactory = previousDefinition.beethovenFactory;
+      implementationObjects = previousDefinition.implementationObjects.Concat(newImplementationObjects).ToArray();
+    }
+
+    public TypeDefinition<T> Add(params object[] newImplementationObjects) =>
+      new TypeDefinition<T>(this, newImplementationObjects);
+
+    public void RegisterEvent(string name, Action<IEventTrigger> triggerFunc) =>
+      eventList.Add(ValueTuple.Create(name, triggerFunc));
+
+    public T Create()
+    {
+      T generated = beethovenFactory.Generate<T>(implementationObjects);
+      eventList.ForEach(tuple => tuple.Item2(beethovenFactory.CreateEventTrigger(generated, tuple.Item1)));
+      return generated;
+    }
+
+    public IEventTrigger CreateEventTrigger(object mainObject, string name) =>
+      beethovenFactory.CreateEventTrigger(mainObject, name);
+  }
+}
