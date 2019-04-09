6cb554949e9810a3afca2c6b48b35864ed875dd6
Alan Hauge Andersen <alan@hauge.com>
2019-04-09 22:04:54 +0200

    Added support for get-only properties

--- GalvanizedBeethoven/Generic/Properties/Mapped.cs
+++ GalvanizedBeethoven/Generic/Properties/Mapped.cs
@@ -8,31 +8,34 @@ namespace GalvanizedSoftware.Beethoven.Generic.Properties
   {
     private readonly object main;
     private readonly string name;
-    private readonly PropertyInfo propertyInfo;
+    private readonly MethodInfo getMethod;
+    private readonly MethodInfo setMethod;
 
     public Mapped(object target, string name)
     {
       this.name = name;
       main = target;
-      propertyInfo = target.GetType().GetProperty(name, ResolveFlags);
+      PropertyInfo propertyInfo = target.GetType().GetProperty(name, ResolveFlags);
+      if (propertyInfo == null)
+        return;
+      getMethod = propertyInfo.CanRead ? propertyInfo.GetMethod : null;
+      setMethod = propertyInfo.CanWrite ? propertyInfo.SetMethod : null;
     }
 
-    // ReSharper disable once RedundantAssignment
     public bool InvokeGetter(ref T returnValue)
     {
-      returnValue = (T)propertyInfo.GetMethod.Invoke(main, new object[0]);
+      if (getMethod != null)
+        returnValue = (T)getMethod.Invoke(main, new object[0]);
       return true;
     }
 
     public bool InvokeSetter(T newValue)
     {
-      propertyInfo.SetMethod.Invoke(main, new object[] { newValue });
+      setMethod?.Invoke(main, new object[] { newValue });
       return true;
     }
 
-    public Property CreateMasterProperty()
-    {
-      return new Property<T>(new Property<T>(name), this);
-    }
+    public Property CreateMasterProperty() =>
+      new Property<T>(new Property<T>(name), this);
   }
 }
