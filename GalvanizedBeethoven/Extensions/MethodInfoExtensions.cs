f071988296f52e57fcf0413ee5b1449a85f11fd1
Alan Hauge Andersen <alan@hauge.com>
2019-06-02 20:32:12 +0200

    Cleanup

--- GalvanizedBeethoven/Extensions/MethodInfoExtensions.cs
+++ GalvanizedBeethoven/Extensions/MethodInfoExtensions.cs
@@ -20,30 +20,26 @@ namespace GalvanizedSoftware.Beethoven.Extensions
         .ToArray();
     }
 
-    public static bool IsMatch(this MethodInfo methodInfo, IEnumerable<(Type, string)> parameters, Type[] genericArguments, Type returnType)
-    {
-      return methodInfo.IsMatch(parameters.Select(tuple => tuple.Item1), genericArguments, returnType);
-    }
+    public static bool IsMatch(this MethodInfo methodInfo, IEnumerable<(Type, string)> parameters, Type[] genericArguments, Type returnType) =>
+      IsMatch(methodInfo, parameters.Select(tuple => tuple.Item1), genericArguments, returnType);
 
     public static bool IsMatch(this MethodInfo methodInfo, IEnumerable<Type> parameters, Type[] genericArguments, Type returnType)
     {
-      MethodInfo actualMethod = methodInfo.IsGenericMethod ? methodInfo.MakeGenericMethod(genericArguments) : methodInfo;
+      MethodInfo actualMethod = methodInfo.GetActualMethod(genericArguments);
       return actualMethod
                .GetParameterTypes()
                .SequenceEqual(parameters) &&
              returnType.FullName?.TrimEnd('&') == actualMethod.ReturnType.FullName;
     }
 
-    public static object Invoke(this MethodInfo methodInfo, object instance, object[] parameters, Type[] genericArguments)
-    {
-      MethodInfo actualMethod = methodInfo.IsGenericMethod ? methodInfo.MakeGenericMethod(genericArguments) : methodInfo;
-      return actualMethod.Invoke(instance, parameters);
-    }
+    public static object Invoke(this MethodInfo methodInfo, object instance, object[] parameters, Type[] genericArguments) =>
+      methodInfo.GetActualMethod(genericArguments).Invoke(instance, parameters);
 
-    public static MethodInfo MakeGeneric<T>(this MethodInfo methodInfo)
-    {
-      return methodInfo.IsGenericMethod ? methodInfo.MakeGenericMethod(typeof(T)) : methodInfo;
-    }
+    private static MethodInfo GetActualMethod(this MethodInfo methodInfo, Type[] genericArguments) =>
+      methodInfo.IsGenericMethod ? methodInfo.MakeGenericMethod(genericArguments) : methodInfo;
+
+    public static MethodInfo MakeGeneric<T>(this MethodInfo methodInfo) =>
+      methodInfo.IsGenericMethod ? methodInfo.MakeGenericMethod(typeof(T)) : methodInfo;
 
     public static bool HasReturnType(this MethodInfo methodInfo)
       => methodInfo.ReturnType != typeof(void);
