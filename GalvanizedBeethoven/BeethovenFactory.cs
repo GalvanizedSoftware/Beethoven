f35053f2459e7a366d45e819b773fc3787ea653a
Alan Hauge Andersen <alan@hauge.com>
2020-05-09 12:18:35 +0200

    Separated compiler part

--- GalvanizedBeethoven/BeethovenFactory.cs
+++ GalvanizedBeethoven/BeethovenFactory.cs
@@ -2,20 +2,16 @@
 using GalvanizedSoftware.Beethoven.Core.CodeGenerators;
 using GalvanizedSoftware.Beethoven.Extensions;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.Emit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.IO;
 using static System.Reflection.Assembly;
 
 namespace GalvanizedSoftware.Beethoven
 {
   public sealed class BeethovenFactory
   {
-    private static int assemblyNumber = 1;
     private static readonly MethodInfo generateMethodInfo = typeof(BeethovenFactory)
       .GetMethods(Constants.ResolveFlags)
       .Where(info => info.Name == nameof(GenerateInternal))
@@ -61,38 +57,11 @@ namespace GalvanizedSoftware.Beethoven
       ClassGenerator classGenerator = new ClassGenerator(type, className, definitions);
       GeneratorContext generatorContext = new GeneratorContext(className, type);
       string code = classGenerator.Generate(generatorContext).Format(0);
-      Assembly[] assemblies = new[]
-      {
-        typeof(object).Assembly,
-        typeof(T).Assembly,
-        Assembly.GetCallingAssembly(),
-      };
-      AssemblyCache<T> assemblyCache = new AssemblyCache<T>(callingAssembly);
-      MetadataReference[] references = assemblyCache
-        .Select(assembly => MetadataReference.CreateFromFile(assembly.Location))
-        .ToArray();
-      SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code);
-      CSharpCompilationOptions options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
-      CSharpCompilation compilation = CSharpCompilation.Create(
-        $"TmpAssembly{++assemblyNumber}",
-        new[] { syntaxTree },
-        references,
-        options);
-      using (MemoryStream assemblyStream = new MemoryStream())
-      using (MemoryStream pbdStream = new MemoryStream())
-      {
-        EmitResult result = compilation.Emit(assemblyStream, pbdStream);
-        string[] erros = result
-          .Diagnostics
-          .Select(error => error.ToString())
-          .ToArray();
-        if (!result.Success)
-          throw new Exception("Internal compilation error");
-        Assembly assembly = Assembly.Load(assemblyStream.ToArray(), pbdStream.ToArray());
-        Type compiledType = assembly.GetType($"{type.Namespace}.{className}");
-        IBindingParent bindingParents = new BindingParents(allPartDefinitions);
-        return new CompiledTypeDefinition<T>(compiledType, bindingParents);
-      }
+      IEnumerable<Assembly> assemblyCache = new AssemblyCache<T>(callingAssembly);
+      Compiler compiler = new Compiler(code, assemblyCache);
+      return new CompiledTypeDefinition<T>(
+        compiler.GenerateAssembly().GetType($"{type.Namespace}.{className}"),
+        new BindingParents(allPartDefinitions));
     }
 
     private IEnumerable<ICodeGenerator> GetAllCodeGenerators(object part) =>
---
+++ GalvanizedBeethoven/Core/Compiler.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using static System.Reflection.Assembly;
+
+namespace GalvanizedSoftware.Beethoven.Core
+{
+  class Compiler
+  {
+    private static int assemblyNumber = 1;
+    private readonly string code;
+    private readonly IEnumerable<Assembly> assemblyCache;
+
+    public Compiler(string code, IEnumerable<Assembly> assemblyCache)
+    {
+      this.code = code;
+      this.assemblyCache = assemblyCache;
+    }
+
+    internal Assembly GenerateAssembly()
+    {
+      MetadataReference[] references = assemblyCache
+        .Select(assembly => MetadataReference.CreateFromFile(assembly.Location))
+        .ToArray();
+      SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code);
+      CSharpCompilationOptions options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+      CSharpCompilation compilation = CSharpCompilation.Create(
+        $"TmpAssembly{++assemblyNumber}",
+        new[] { syntaxTree },
+        references,
+        options);
+      using MemoryStream assemblyStream = new MemoryStream();
+      using MemoryStream pbdStream = new MemoryStream();
+      EmitResult result = compilation.Emit(assemblyStream, pbdStream);
+      string[] erros = result
+        .Diagnostics
+        .Select(error => error.ToString())
+        .ToArray();
+      return result.Success ?
+        Load(assemblyStream.ToArray(), pbdStream.ToArray()) :
+        throw new Exception("Internal compilation error"); ;
+    }
+  }
+}
