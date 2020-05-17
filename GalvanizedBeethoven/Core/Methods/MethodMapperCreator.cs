60acb1f77229a99d265dc7639e2c17c9214b8e92
Alan Hauge Andersen <alan@hauge.com>
2020-05-17 16:50:34 +0200

    Cleanup

--- GalvanizedBeethoven/Core/Methods/MethodMapperCreator.cs
+++ GalvanizedBeethoven/Core/Methods/MethodMapperCreator.cs
@@ -1,8 +1,6 @@
-﻿using GalvanizedSoftware.Beethoven.Core;
-using GalvanizedSoftware.Beethoven.Extensions;
+﻿using GalvanizedSoftware.Beethoven.Extensions;
 using GalvanizedSoftware.Beethoven.Generic.Methods;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,12 +8,10 @@ namespace GalvanizedSoftware.Beethoven.Core.Methods
 {
   internal class MethodMapperCreator<TMain, TChild> : IDefinitions where TMain : class
   {
-    private readonly Func<TMain, TChild> creatorFunc;
     private readonly List<MappedMethodDelayed> methods;
 
     public MethodMapperCreator(Func<TMain, TChild> creatorFunc)
     {
-      this.creatorFunc = creatorFunc;
       methods = new List<MappedMethodDelayed>(typeof(TChild)
           .GetNotSpecialMethods()
           .Select(methodInfo => new MappedMethodDelayed(methodInfo, (target) => creatorFunc(target as TMain))));
