9871175698974dba4aef506893f08d2f4603a8ca
Alan Hauge Andersen <alan@hauge.com>
2020-09-27 20:21:48 +0200

    Added Beethoven.Build project

---
+++ GalvanizedSoftware.Beethoven.Build/BeethovenBuildTask.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Build.Utilities;
+using System;
+
+namespace GalvanizedSoftware.Beethoven.Build
+{
+  public class BeethovenBuildTask : Task
+  {
+    public override bool Execute()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
